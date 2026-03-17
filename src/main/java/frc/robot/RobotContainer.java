c6180ee7f8823b4dcdac64ba9f14d1d558713d63
Dixie Cooper <dcooper@explora.us>
2026-03-17 17:26:07 -0600

    autonmous 3/17

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -156,7 +156,7 @@ SwerveInputStream driveAngularVelocityKeyboard = SwerveInputStream.of(driveBase.
     // This method loads the auto when it is called, however, it is recommended
     // to first load your paths/autos when code starts, then return the
     // pre-loaded auto/path
-    return new PathPlannerAuto("finalAuto");
+    return new PathPlannerAuto("Example Auto");
   }
 }
 
